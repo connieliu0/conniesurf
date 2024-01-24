75f5101832b45f806bd0491cb02cc2bb6d82d455
Connie <cl2264@cornell.edu>
2024-01-24 00:19:45 -0500

    almost there

--- abscript.js
+++ abscript.js
@@ -3,40 +3,55 @@ const fixedDiv = document.getElementById("dyn");
 const topsec = fixedDiv.querySelector("#tight");
 const center = fixedDiv.querySelector("#center");
 const bottom = fixedDiv.querySelector("#bottom");
-let topContent = "";
-let centerContent = "";
-let bottomContent = "";
+let topContent = topsec.innerHTML;
+let centerContent = center.innerHTML;
+let bottomContent = bottom.innerHTML;
+let sectionId = "";
+var isKeyPressed = false;
 document.addEventListener("DOMContentLoaded", function () {
   fetch("abdata.json")
     .then((response) => response.json())
     .then((nowmap) => {
+      document.addEventListener("keydown", function (event) {
+        if (event.key === "i") {
+          isKeyPressed = true;
+        }
+      });
+      document.addEventListener("keyup", function (event) {
+        isKeyPressed = false;
+      });
       const spans = scrollDiv.querySelectorAll("span");
       spans.forEach((span) => {
         const spanId = span.id;
         span.addEventListener("mouseover", function () {
           if (nowmap[spanId]) {
-            topContent = topsec.innerHTML;
-            centerContent = center.innerHTML;
-            bottomContent = bottom.innerHTML;
             topsec.innerHTML = nowmap[spanId].top;
             center.innerHTML = nowmap[spanId].center;
             bottom.innerHTML = nowmap[spanId].bottom;
           }
         });
         span.addEventListener("mouseout", function () {
-          topsec.innerHTML = topContent;
-          center.innerHTML = centerContent;
-          bottom.innerHTML = bottomContent;
+          if (isKeyPressed) {
+            topsec.innerHTML = nowmap[spanId].top;
+            center.innerHTML = nowmap[spanId].center;
+            bottom.innerHTML = nowmap[spanId].bottom;
+          } else {
+            topsec.innerHTML = topContent;
+            center.innerHTML = centerContent;
+            bottom.innerHTML = bottomContent;
+          }
         });
       });
       scrollDiv.addEventListener("scroll", function () {
-        const scrollPosition = scrollDiv.scrollTop;
+        var scrollPosition = scrollDiv.scrollTop;
         const sections = scrollDiv.querySelectorAll(".row");
+        var viewportHeight = scrollDiv.clientHeight;
+        var totalHeight = scrollDiv.scrollHeight;
+
         sections.forEach((section) => {
           const offset = section.offsetTop;
           const height = section.offsetHeight;
-          const sectionId = section.id;
-
+          sectionId = section.id;
           if (
             scrollPosition >= offset - 12 &&
             scrollPosition < offset + height
@@ -45,7 +60,14 @@ document.addEventListener("DOMContentLoaded", function () {
               topsec.innerHTML = nowmap[sectionId].top;
               center.innerHTML = nowmap[sectionId].center;
               bottom.innerHTML = nowmap[sectionId].bottom;
+              topContent = topsec.innerHTML;
+              centerContent = center.innerHTML;
+              bottomContent = bottom.innerHTML;
             }
+          } else if (scrollPosition + viewportHeight >= totalHeight) {
+            topsec.innerHTML = nowmap["past"].top;
+            center.innerHTML = nowmap["past"].center;
+            bottom.innerHTML = nowmap["past"].bottom;
           }
         });
       });
